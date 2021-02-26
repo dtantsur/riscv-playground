9841b6b9913cf8f87c8a64fba65ef8a2a80ed3f9
Dmitry Tantsur <dtantsur@protonmail.com>
2021-02-26 22:16:08 +0100

    Write hello world

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,30 @@
 
 extern crate panic_halt;
 
-use hifive1::hal::prelude::*;
-use hifive1::hal::DeviceResources;
-use hifive1::hal::delay::Sleep;
+use hifive1::hal::{
+    delay::Sleep, e310x::I2C0, gpio::gpio0::Pin12, gpio::gpio0::Pin13, gpio::NoInvert,
+    gpio::Output, gpio::Regular, gpio::IOF0, i2c::I2c, i2c::Speed, DeviceResources,
+};
+use hifive1::hal::{gpio::gpio0::Pin11, prelude::*};
 use hifive1::{self, pin, Led};
 use riscv_rt::entry;
+use shared_bus::{BusManagerSimple, I2cProxy};
+use ssd1306::{
+    displaysize::DisplaySize128x64, mode::TerminalMode, prelude::I2CInterface, I2CDIBuilder,
+};
 
+type HiFiveI2c = I2c<I2C0, (Pin12<IOF0<NoInvert>>, Pin13<IOF0<NoInvert>>)>;
+
+type DisplayInterface<'b> = I2CInterface<I2cProxy<'b, shared_bus::NullMutex<HiFiveI2c>>>;
+
+type Terminal<'b> = TerminalMode<DisplayInterface<'b>, DisplaySize128x64>;
+
+#[allow(unused)]
 struct Board {
-    pub sleep: Sleep,
-    pub leds: (hifive1::RED, hifive1::GREEN, hifive1::BLUE),
+    sleep: Sleep,
+    leds: (hifive1::RED, hifive1::GREEN, hifive1::BLUE),
+    i2c: BusManagerSimple<HiFiveI2c>,
+    out: Pin11<Output<Regular<NoInvert>>>,
 }
 
 impl Board {
@@ -28,7 +43,15 @@ impl Board {
             115_200.bps(),
             clocks,
         );
-        let leds = hifive1::rgb(pin!(pins, led_red), pin!(pins, led_green), pin!(pins, led_blue));
+        let leds = hifive1::rgb(
+            pin!(pins, led_red),
+            pin!(pins, led_green),
+            pin!(pins, led_blue),
+        );
+        let sda = pin!(pins, i2c0_sda).into_iof0();
+        let scl = pin!(pins, i2c0_scl).into_iof0();
+        let i2c = BusManagerSimple::new(I2c::new(p.I2C0, sda, scl, Speed::Normal, clocks));
+        let out = pin!(pins, dig17).into_output();
 
         let clint = dr.core_peripherals.clint;
         let sleep = Sleep::new(clint.mtimecmp, clocks);
@@ -36,10 +59,20 @@ impl Board {
         let mut result = Board {
             sleep,
             leds,
+            i2c,
+            out,
         };
 
         result.light(false, false, false);
         result
+    }
+
+    pub fn display(&self) -> Terminal {
+        let iface = I2CDIBuilder::new().init(self.i2c.acquire_i2c());
+        let mut display: TerminalMode<_, _> = ssd1306::Builder::new().connect(iface).into();
+        display.init().unwrap();
+        let _ = display.clear();
+        display
     }
 
     pub fn light(&mut self, red: bool, green: bool, blue: bool) {
@@ -70,6 +103,8 @@ impl Board {
 #[entry]
 fn main() -> ! {
     let mut board = Board::new();
+    let mut display = board.display();
+    display.write_str("Hello world").unwrap();
 
     let mut index = 1;
     loop {

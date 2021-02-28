3dcde97038180f860b7f21ac01d169c8bbb59460
Dmitry Tantsur <dtantsur@protonmail.com>
2021-02-28 11:41:28 +0100

    Prepare for temperature readings

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@
 
 extern crate panic_halt;
 
+use bme280::BME280;
+use embedded_hal::blocking::delay::DelayMs;
+use heapless::String;
 use hifive1::hal::{
     delay::Sleep, e310x::I2C0, gpio::gpio0::Pin12, gpio::gpio0::Pin13, gpio::NoInvert,
     gpio::Output, gpio::Regular, gpio::IOF0, i2c::I2c, i2c::Speed, DeviceResources,
@@ -10,7 +13,7 @@ use hifive1::hal::{
 use hifive1::hal::{gpio::gpio0::Pin11, prelude::*};
 use hifive1::{self, pin, Led};
 use riscv_rt::entry;
-use shared_bus::{BusManagerSimple, I2cProxy};
+use shared_bus::BusManagerSimple;
 use ssd1306::{
     displaysize::DisplaySize128x64, mode::TerminalMode, prelude::I2CInterface, I2CDIBuilder,
 };
@@ -19,9 +22,9 @@ use ssd1306::{
 
 type HiFiveI2c = I2c<I2C0, (Pin12<IOF0<NoInvert>>, Pin13<IOF0<NoInvert>>)>;
 
-type DisplayInterface<'b> = I2CInterface<I2cProxy<'b, shared_bus::NullMutex<HiFiveI2c>>>;
+type I2cProxy<'b> = shared_bus::I2cProxy<'b, shared_bus::NullMutex<HiFiveI2c>>;
 
-type Terminal<'b> = TerminalMode<DisplayInterface<'b>, DisplaySize128x64>;
+type Terminal<'b> = TerminalMode<I2CInterface<I2cProxy<'b>>, DisplaySize128x64>;
 
 // Peripherals
 
@@ -58,12 +61,19 @@ impl Leds {
 struct I2cBus(BusManagerSimple<HiFiveI2c>);
 
 impl I2cBus {
-    pub fn display(&self) -> Terminal {
+    fn display(&self) -> Terminal {
         let iface = I2CDIBuilder::new().init(self.0.acquire_i2c());
         let mut display: TerminalMode<_, _> = ssd1306::Builder::new().connect(iface).into();
         display.init().unwrap();
         let _ = display.clear();
         display
+    }
+
+    fn temperature_sensor<D: DelayMs<u8>>(&self, delay: &mut D) -> BME280<I2cProxy> {
+        let mut bme = BME280::new_primary(self.0.acquire_i2c());
+        let result = bme.init(delay);
+        result.unwrap();
+        bme
     }
 }
 
@@ -124,10 +134,20 @@ impl Board {
 fn main() -> ! {
     let mut board = Board::new();
     let mut display = board.i2c.display();
+
     display.write_str("Hello world").unwrap();
+
+    // let mut lines: [String<heapless::consts::U32>; 2] = [String::new(), String::new()];
 
     let mut index = 1;
     loop {
+        // if let Ok(msr) = temp.measure(&mut board.sleep) {
+        //     lines.iter_mut().for_each(String::clear);
+        //     write!(lines[0], "t {:.1}, h {:.1}%", msr.temperature, msr.humidity).unwrap();
+        //     display.clear().unwrap();
+        //     display.write_str(lines[0].as_str()).unwrap();
+        // }
+
         if index % 2 == 1 {
             board
                 .leds
@@ -135,7 +155,7 @@ fn main() -> ! {
         } else {
             board.leds.light(false, false, false);
         }
-        board.sleep.delay_ms(500u32);
+        board.sleep.delay_ms(1000u32);
         index = index % 15 + 1;
     }
 }
